41448ebb5ef00f4f6e8b026e6e7acee018c6a9e3
son chae min <sonchaemin89@gmail.com>
2023-09-20 20:26:10 +0900

    part2.db읽어오기

--- server.js
+++ server.js
@@ -12,23 +12,19 @@ MongoClient.connect('mongodb+srv://sonchaemin89:e0e867e6^^**@molcham.9u8swtc.mon
 
     db=client.db('todoapp');
 
-    
+    db.collection('post').insertOne({이름:'채민',나이:22},function(에러,결과){
+      console.log('저장완료');
+    });
+
+
     app.listen('8080', function(){
-            console.log('listening on 8080')
-          });
+      console.log('listening on 8080')
+     });
     
   })
 
 /*누군가가 /pet으로 방문을 하면..
   pet 관련 안내문을 띄워주자*/
-
-app.get('/beauty',function(요청,응답){
-    응답.send('뷰티용품을 쇼핑할 수 있는 페이지입니다.');
-});
-
-app.get('/pet',function(요청,응답){
-    응답.send('펫 용품을 쇼핑할 수 있는 페이지입니다.');
-});
 
 app.get('/',function(요청,응답){
     응답.sendFile(__dirname+'/write.html');
@@ -36,21 +32,24 @@ app.get('/',function(요청,응답){
 
 app.post('/add', function(요청, 응답){
     응답.send('전송완료');
+    console.log(요청.body.date);
+    console.log(요청.body.title);
     db.collection('post').insertOne( { 제목 : 요청.body.title, 날짜 : 요청.body.date } , function(){
       console.log('저장완료')
     });
   });
 
-  app.get('/list',function(요청,응답){
+  /*/list로 GET요청으로 접속하면
+     실제 DB에 저장된 데이터들로 예쁘게 꾸며진 html을 보여줌 
+  */
+app.get('/list',function(요청,응답){
 
 
-
-    db.collection('post').find().toArray(function(에러,결과){
-      console.log(결과);
-      응답.render('list.ejs',{post:결과});
-    });
-    
+  db.collection('post').find().toArray(function(에러,결과){
+    console.lof(결과);
   });
+  응답.render('list.ejs');
+});
+
 
     
-
