b64fea26a7ffd5b4730b223e60066367d13b9e05
son chae min <sonchaemin89@gmail.com>
2023-09-26 13:34:38 +0900

    part2.ajax로 취소요청하기

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const bodyParser= require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs');
 const MongoClient = require('mongodb').MongoClient;
-
+const uri = 'mongodb://localhost:27017'; 
+const client = new MongoClient(uri, { useUnifiedTopology: true });
 var db;
 MongoClient.connect('mongodb+srv://sonchaemin89:e0e867e6^^**@molcham.9u8swtc.mongodb.net/?retryWrites=true&w=majority', function(에러, client){
     if (에러) return console.log(에러);
@@ -31,11 +32,14 @@ app.post('/add', function(요청, 응답){
     console.log(요청.body.date);
     console.log(요청.body.title);
     db.collection('counter').findOne({name:'게시물갯수'},function(에러,결과){
-      console.log(결과.totalPost)
-      var 총게시물갯수=결과.totalPost;
+      console.log(결과.totalPost);
+      let 총게시물갯수=결과.totalPost;
 
       db.collection('post').insertOne( { _id : 총게시물갯수+1, 제목 : 요청.body.title, 날짜 : 요청.body.date } , function(){
         console.log('저장완료')
+        //counter라는 콜렉션에 있는 totalPost라는 항목도 1증가 시켜야함
+        db.collection('counter').updateOne({name:'게시물갯수'},{$inc:{totalPost:1}})
+
       });
 
     });
@@ -53,6 +57,21 @@ app.get('/list',function(요청,응답){
     응답.render('list.ejs',{posts:결과});
   });
 });
+
+app.delete('/delete',function(요청,응답){
+  console.log(요청.body);
+  요청.body._id=parseInt(요청.body._id);
+  //요청.body에 담겨온 게시물번호를 가진 글을 db에서 찾아서 삭제해주세요
+  db.collection('post').deleteOne(요청.body,function(에러,결과){
+    console.log('삭제완료');
+
+  })
+})
+
+
+app.get('/edit',function(요청,응답){
+  응답.render('edit.ejs')
+})
 
 
     
